b267316751fce051fb6c9c1f0577991585f51764
Jodie <jodieeagleson@gmail.com>
2021-11-07 08:50:54 -0500

    updates readme

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # recipe-cron
 
-You will need:
+This is a small node.js program that sends recipe reminders to the user, so they do not forget about food they enjoy making.
 
+###### You will need:
 cron.js
 axios.js
 dotenv.js
 
-Add a .env file with the following structure:
+###### Default interval:
+Every second
 
+###### Add a .env file with the following structure:
 DA_HOOK="your webhook url"
--- file.js
+++ file.js
@@ -2,13 +2,13 @@ const dotenv = require('dotenv').config()
 const CronJob = require('cron').CronJob;
 const axios = require('axios')
 
-let recipes = ["squash pasta", "lettuce wraps", "shepherds pie", "burritos", "just candy", "carrot soup", "risotto", "jambalaya", "cauliflower wings", "zucchini noodle pasta", "fancy sandwiches", "french toast"];
+let recipes = ["Squash pasta", "Lettuce wrap", "Monkey Brains", "Shepherds pie", "Burrito", "Just candy", "Carrot soup", "Risotto", "Jambalaya", "Cauliflower wings", "Zucchini noodle pasta", "Fancy sandwiches", "French toast", "Tomato salad"];
 
 const recipeSelector = () => {
     return recipes[Math.floor(Math.random()*recipes.length)];
 }
 
-var job = new CronJob('* * */3 * *', function() {
+var job = new CronJob('* * * * * *', function() {
   console.log(sendFoodToDiscord());
 }, null, true, 'America/Los_Angeles');
 console.log('After job instantiation');
@@ -19,7 +19,7 @@ axios
 
   .post(process.env.DA_HOOK, {
     content: 
-    "Don't forget this tasty nugget: " +
+    "Don't forget this tasty nugget:  " +
     recipeSelector()
   })
   .then(res => {
